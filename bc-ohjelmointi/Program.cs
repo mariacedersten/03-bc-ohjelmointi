1f6d16748ade248e854af3a167bcbe928e695ae8
mariacedersten <s2401554@edu.bc.fi>
2025-05-11 21:24:33 +0300

    Tehty

---
+++ bc-ohjelmointi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace BC_Ohjelmointi
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna numero:");
+            int numero = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(numero + "*" + i + "=" + numero * i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
